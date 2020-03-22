3cac4b738d285af20a377f2801f68856ce5925e0
Nandini3103 <61120008+Nandini3103@users.noreply.github.com>
2020-03-22 20:51:04 +0530

    Initial Commit

---
+++ I AM POOR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by Nandini Aggarwal on 22/03/20.
+//  Copyright © 2020 Nandini Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
